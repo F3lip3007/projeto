cc50e7a41b5f9fbe64a675462fa62fa89251a86f
F3lip3007 <gomesfelipeg123@gmail.com>
2024-11-04 10:57:43 -0300

    felipe
    deletar (fazer o cascata)

--- controle/deletar.php
+++ controle/deletar.php
@@ -1,30 +1,27 @@
 <?php
 require_once "./conexao.php";
 
-$tabela = $_GET['$tabela'];
+$tabela = $_GET['tabela'];
 $campo = $_GET['campo'];
 $loc = $_GET['loc'];
 // url
 $id = $_GET['id'];
 
-echo'$campo';
-echo'$loc';
-echo'$id ';
-echo'$tabela';
+// echo $campo;
+// echo $loc;
+// echo $id ;
+// echo $tabela;
 
 // DELECT FROM paciente WHERE idpaciente =2;
-$sql = "DELECT FROM  $tabela WHERE $campo = $id";
+$sql = "DELECT FROM $tabela WHERE $campo = $id";
 
-mysqli_query($conexao, $sql);
 
 header("Location: ../public/$loc");
-?><?php
-    require_once "./conexao.php";
 
     $id = $_GET['id'];
     
     // DELETE FROM paciente WHERE idpaciente = 3;
-    $sql = "DELETE FROM $tabe WHERE idcliente = $id;";
+    $sql = "DELETE FROM $tabela WHERE idcliente = $id;";
 
     mysqli_query($conexao, $sql);
 
--- public/clientesql.php
+++ public/clientesql.php
@@ -53,7 +53,7 @@ require_once "../controle/verificalogado.php";
                 echo "<td>$nome</td>";
                 echo "<td>$telefone</td>";
                 echo "<td>$nascimento</td>";
-                echo "<td> <a class = 'letra' href='../controle/deletar.php?id=$id & campo=$campo & tabela=$tabela & loc=$loc'>deletar</a><td/>";
+                echo "<td> <a class = 'letra' href='../controle/deletar.php?id=$id&campo=$campo&tabela=$tabela&loc=$loc'>deletar</a><td/>";
                 echo "</tr>";
             }
         ?>
