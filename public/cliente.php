8240eda6ea73ef8e357414cfe8221017efb1ede2
julia <juliaasouzaviana@gmail.com>
2024-11-18 16:28:41 -0300

    julia
    cass cliente

--- public/cliente.php
+++ public/cliente.php
@@ -1,4 +1,4 @@
-<?php
+<div?php
 require_once "../controle/verificalogado.php";
 
 require_once "../controle/conexao.php"; // Conexão com o banco de dados
@@ -47,26 +47,42 @@ if (isset($_GET['idcliente'])) {
     <title><?php echo $titilo; ?></title>
     <link rel="stylesheet" href="./css/estilo.css">
 </head>
-<body>
-    <h1><?php echo $titilo; ?></h1>
+<>
+    <div class ="form-container2"></div>
+    <div class = "form-box"></div>
 
+    <h1 class ="tituloc"> <?php echo $titilo; ?> </h1>  
+       
     <form action="../controle/editarcliente.php" method="POST">
         <input type="hidden" name="idcliente" value="<?php echo $idcliente; ?>"> <!-- Campo oculto para ID -->
 
+    <div class = "form-group">
         <h3>Nome do cliente:</h3>
         <input type="text" placeholder="Nome" name="nome" value="<?php echo $nome; ?>"> <br><br>
+    </div>
 
+    <div class = "form-group">
         <h3>Data de nascimento:</h3>
         <input type="date" name="data_de_nascimento" value="<?php echo $data_de_nascimento; ?>"> <br><br>
+    </div>
 
+    <div class = "form-group">
         <h3>CPF:</h3>
         <input type="text" placeholder="CPF" name="cpf" value="<?php echo $cpf; ?>"><br><br>
+    </div>
 
+    <div class = "form-group">
         <h3>Telefone:</h3>
         <input type="text" placeholder="Telefone" name="telefone" value="<?php echo $telefone; ?>"><br><br>
+    </div>
 
-        <input type="submit" value="<?php echo $botao; ?>">
+        <input type="submit" class ="btn-registrer" value="<?php echo $botao; ?>">
     </form>
+</div>
+
+<div class ="form-image">
+    <div class = "rocket"></div>
+</div>
 </body>
 
 </html>
